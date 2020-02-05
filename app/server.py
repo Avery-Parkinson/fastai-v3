b61351bcdd4b7f97894e2a143c1494a1b1f55113
Avery-Parkinson <60632985+Avery-Parkinson@users.noreply.github.com>
2020-02-05 18:12:19 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://colab.research.google.com/drive/1rWHgwqISaHme_cGiBqJYCTXUAKwIoS9E'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-1Q0ePoK4C_4tRcJlALU0eBflx3JYOhe'
 export_file_name = 'export.pkl'
 
 classes = ['cardboard', 'paper', 'plastic', 'metal', 'trash']
